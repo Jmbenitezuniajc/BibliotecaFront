8109acb5a5fd167237b370db31ceac924ac26d62
santiago-ross <santiagopradocarabali.spc@gmail.com>
2022-05-16 03:15:32 -0500

    partes de servicios

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Login/Login';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Login/Login';
+import Inicio from './Inicio/Inicio';
+import AgregarAutor from './AgregarAutor/AgregarAutor';
+import AgregarUsuario from './AgregarUsuario/AgregarUsuario';
+import AgregarLibro from "./AgregarLibro/AgregarLibro";
+import GestionarUsuario from "./GestionarUsuario/GestionarUsuario";
+import GestionarLibro from "./GestionarLibro/GestionarLibro";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+  <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="inicio" element={<Inicio />} />
+      <Route path="agregarusuario" element={<AgregarUsuario />} />
+      <Route path="agregarautor" element={<AgregarAutor />} />
+      <Route path="agregarlibro" element={<AgregarLibro />} />
+      <Route path="gestionarusuario" element={<GestionarUsuario />} />
+      <Route path="gestionarlibro" element={<GestionarLibro />} />
+    </Routes>
+</BrowserRouter>
   </React.StrictMode>
 );
 
